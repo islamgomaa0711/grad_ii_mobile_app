48597fb7466ba1397bad05875a3b0dc294211ca0
islamgomaa0711 <islam.gomaa.fotouh@gmail.com>
2025-06-29 15:04:18 +0300

    welcome_screen.dart

--- lib/welcome_screen.dart
+++ lib/welcome_screen.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'package:provider/provider.dart';
 import 'package:easy_localization/easy_localization.dart';
-import 'login_screen.dart';
-import 'theme_provider.dart';
-import 'package:shared_preferences/shared_preferences.dart';
+
 
 
 class WelcomeScreen extends StatefulWidget {
